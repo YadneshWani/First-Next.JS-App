c26b272611afcc6d80f9f59ea8ff065b0f858ec3
YadneshWani <yadneshwani0987@gmail.com>
2022-07-26 10:47:53 +0530

    Changes made in about.js

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ export default function about() {
     return (
       <div >
         <h1>ABOUT PAGE</h1>
+        <p>Hi I am Yadnesh Wani, Pursuing my BE in Computer Engineering at D Y Patil College of Engineering,Akurdi, Pune</p>
       </div>
     )
   }
